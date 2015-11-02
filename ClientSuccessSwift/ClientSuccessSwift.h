9796529c64bfac4a123d04d06ee1bec60f3c8053
Jose Luis Matus <josem@mediarain.com.ni>
2015-11-02 10:02:08 -0600

    Initial Commit

---
+++ ClientSuccessSwift/ClientSuccessSwift.h
@@ -0,0 +1,19 @@
+//
+//  ClientSuccessSwift.h
+//  ClientSuccessSwift
+//
+//  Created by Jose on 11/2/15.
+//  Copyright © 2015 Rain Agency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClientSuccessSwift.
+FOUNDATION_EXPORT double ClientSuccessSwiftVersionNumber;
+
+//! Project version string for ClientSuccessSwift.
+FOUNDATION_EXPORT const unsigned char ClientSuccessSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClientSuccessSwift/PublicHeader.h>
+
+
